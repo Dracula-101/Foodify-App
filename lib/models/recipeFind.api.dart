6c589a0122e8bee0df52376286f6001ca1178eda
Dracula-101 <66874986+Dracula-101@users.noreply.github.com>
2022-03-19 09:54:29 +0530

    Added Tflite

---
+++ lib/models/recipeFind.api.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:foodify/models/recipe.dart';
+import 'package:http/http.dart' as http;
+import 'package:foodify/constants/key.dart';
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https(BASE_URL, '/recipes/random', {
+      "number": items.toString(),
+      ""
+          "apiKey": API_KEY,
+    });
+
+    final response = await http
+        .get(uri, headers: {"x-api-key": API_KEY, "useQueryString": "true"});
+
+    Map data = jsonDecode(response.body);
+    if (data['code'] == 402) {
+      changeAPiKey();
+      return getRecipe();
+    }
+    List _temp = [];
+
+    for (var i in data['recipes']) {
+      _temp.add(i);
+    }
+    // print(_temp);
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
